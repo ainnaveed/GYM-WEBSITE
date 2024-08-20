49def24f21af042580ac2991f3ed7a4fae75c9f2
Zainnaveed <zainnaveed4307@gmail.com>
2024-08-20 12:20:32 +0500

    first commit

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <header>
+        <p>ZN EDGE FITNESS</p>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
